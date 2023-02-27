60d38c8f19090d35ab97d1034fbaeb96e09534d8
paulmunyao <paulmunyao094@gmail.com>
2023-02-27 17:02:31 +0300

    use await in async when returning a promise

--- promises.ts
+++ promises.ts
@@ -1,5 +1,6 @@
 // run async that returns a promise and resolves when some background work is complete 
 async function myFunction(){
     await doAsyncWork()
+    // A promise is returned from the async function
     return aPromise
 }
