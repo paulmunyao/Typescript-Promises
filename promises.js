46fc29f373b995cb65650c85214c0a3ae1306569
paulmunyao <paulmunyao094@gmail.com>
2023-02-28 23:48:25 +0300

    comment out unused code

--- promises.js
+++ promises.js
@@ -3,7 +3,7 @@
 // await doAsyncWork()
 // A promise is returned from the async function
 // return aPromise
-// }
+
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
