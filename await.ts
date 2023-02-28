50303062dbb687df934d33cb7f1d8d952c74c2d4
paulmunyao <paulmunyao094@gmail.com>
2023-02-28 22:54:58 +0300

    indent first result

--- await.ts
+++ await.ts
@@ -1,15 +1,15 @@
-let promiseOne = new Promise((resolve ,reject)=>{
-    setTimeout(()=> resolve("Resolve promise earlier"))
+let promiseOne = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("Resolve promise earlier"));
 });
 
-let promiseTwo  = new Promise((resolve, reject)=>{
-    setTimeout(()=> resolve("Resolve promise later on"))
+let promiseTwo = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("Resolve promise later on"));
 });
 
-async function asyncAwaitFunction (): Promise<any>{
-    let firstResult = await promiseOne;
-    let secondResult = await promiseTwo;
-    return firstResult
+async function asyncAwaitFunction(): Promise<any> {
+  let firstResult = await promiseOne;
+  let secondResult = await promiseTwo;
+  return firstResult;
 }
 
-asyncAwaitFunction ().then (result => console.log(result));
+asyncAwaitFunction().then((result) => console.log(result));
