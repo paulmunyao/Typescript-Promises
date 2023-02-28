28f072fc997356658bcb1a09478ee84e857c5761
paulmunyao <paulmunyao094@gmail.com>
2023-02-28 22:29:56 +0300

    end the functions by closing with semicolons

--- await.ts
+++ await.ts
@@ -1,6 +1,6 @@
 let promiseOne = new Promise((resolve ,reject)=>{
     setTimeout(()=> resolve("Resolve promise earlier"))
-})
+});
 
 let promiseTwo  = new Promise((resolve, reject)=>{
     setTimeout(()=> resolve("Resolve promise later on"))
